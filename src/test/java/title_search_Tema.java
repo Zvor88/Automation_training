843c276e529fc0c4cd2d35d99f54382bb156c260
zbura <zburatorul.g@gmail.com>
2026-03-01 21:54:00 +0200

    Tema mmodificari

--- src/test/java/title_search_Tema.java
+++ src/test/java/title_search_Tema.java
@@ -14,11 +14,12 @@ public class title_search_Tema {
     public void setup(){
         driver = new ChromeDriver();
         driver.get("https://www.imd.org/degree/mba/");
+        driver.findElement(By.xpath("//button[contains(text(), 'Agree & Proceed')]"));;
     }
 
     @Test
     public void searchTest() {
-        driver.findElement(By.xpath("//button[contains(text(), 'Agree & Proceed')]"));;
+
 
         System.out.println(driver.getTitle());
         String actualTitle = driver.getTitle();
@@ -27,9 +28,18 @@ public class title_search_Tema {
         Assert.assertEquals(actualTitle, expectedTitle, "Verify the page title.");
 
     }
+    @Test
+    public void searchTest2() {
+        String actualTitle = driver.getTitle();
+        System.out.println("Page Title is: " + actualTitle);
 
+        Assert.assertNotNull(actualTitle, "The page title should not be null.");
 
+        Assert.assertFalse(actualTitle.isEmpty(), "The page title should not be empty.");
 
+        String keyword = "MBA";
+        Assert.assertTrue(actualTitle.contains(keyword),
+                "The title does not contain the expected keyword: " + keyword);
     @AfterClass
     public void tearDown() {
         driver.quit();
